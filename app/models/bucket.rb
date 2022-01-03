344a866121fa47b9cf0d4993478d57bfd7d75da3
Joshua Burger <joshberger6969@outlook.com>
2022-01-03 16:40:22 -0500

    has_many tasks association, get by status method

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -1,2 +1,11 @@
 class Bucket < ApplicationRecord
+    has_many :tasks
+
+    def self.get_by_status(status)
+        self.all.select do |bucket|
+            bucket.status == status
+        end
+    end
+
+    
 end
