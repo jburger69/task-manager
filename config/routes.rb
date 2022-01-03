7aa3756130ce15ca36f6486d6b854ecb3b3f94d6
Joshua Burger <joshberger6969@outlook.com>
2022-01-03 17:10:16 -0500

    root buckets show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'buckets_controller#show'
   resources :tasks
   resources :buckets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
